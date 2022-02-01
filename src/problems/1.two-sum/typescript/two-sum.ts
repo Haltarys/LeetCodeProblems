77cc67aed3e1930e2bee76453f51935f1f1623f3
Haltarys <gregoire.haardt@gmail.com>
2022-01-31 16:35:54 -0800

    perf(1.two-sum): improve brute-force algorithm"
    Skip redundant and out-right invalid pairs.

--- src/problems/1.two-sum/typescript/two-sum.ts
+++ src/problems/1.two-sum/typescript/two-sum.ts
@@ -1,7 +1,9 @@
 function twoSum(nums: number[], target: number): number[] {
   // for each possible pair of numbers
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
+    // excluding using the same elements more than once (main diagonal)
+    // and symmetrical of pairs ([a, b] == [b, a])
+    for (let j = i + 1; j < nums.length; j++) {
       // if the numbers add up to the target and are not the same element
       if (nums[i] + nums[j] == target && i != j) {
         return [i, j];
