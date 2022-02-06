c249527d90a2e1f247c66161c991d5a0ee8fc749
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-02-06 08:50:03 +0100

    Dev (#9)
    * ci: freeze version of yarn to use
    
    * chore(lint): add extra rules for linting
    
    * ci(jest): improve jest command for CI tests
    
    * chore(lint): deprecates the use of 'console'
    
    * build: omit comment and require explicit return statements

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'warn',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'warn',
   },
 };
