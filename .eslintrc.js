e5f216d1278f6f5e6354b0194bbcb362baf9b0aa
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-02-06 07:52:16 +0100

    Dev (#8)
    * ci: freeze version of yarn to use
    
    * chore(lint): add extra rules for linting
    
    * ci(jest): improve jest command for CI tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,4 +7,9 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
