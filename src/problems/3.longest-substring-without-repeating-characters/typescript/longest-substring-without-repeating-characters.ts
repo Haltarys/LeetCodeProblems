899e6a949f89dcc7ead43a9d531a278f6b401c1e
Haltarys <gregoire.haardt@gmail.com>
2022-02-15 17:07:59 -0800

    WIP: feat(3.longest-substring-without-repeating-characters): add tests and filler function

---
+++ src/problems/3.longest-substring-without-repeating-characters/typescript/longest-substring-without-repeating-characters.ts
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=3 lang=typescript
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+/**
+ * Given a string `s`, returns the length of the longest substring without repeating characters.
+ *
+ * @param s A string of characters.
+ * @returns The length of the longest substring in `s` without repeating characters.
+ */
+
+// @lc code=start
+function lengthOfLongestSubstring(s: string): number {
+  return s.length;
+}
+// @lc code=end
+
+export { lengthOfLongestSubstring };
