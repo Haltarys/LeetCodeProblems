e1c0a2bea9c06ff5a87ec5bcd7dde74944099498
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-07-11 18:05:02 +0200

    3.longest substring without repeating characters (#19)
    * WIP: feat(3.longest-substring-without-repeating-characters): add tests and filler function
    
    * test(3.longest-substring-without-repeating-characters): add test for single character string
    
    * style(3.longest-substring-without-repeating-characters): remove blank lines for consistency
    
    * feat(3.longest-substring-without-repeating-characters): implement iterative solution
    
    * docs(3.longest-substring-without-repeating-characters): add documentation for the problem's solution
    
    * style: add solved problem to list

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ A repository containing my solutions to [LeetCode's problems set](https://leetco
 
 1. [Two Sum](https://leetcode.com/problems/two-sum/)
 2. [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)
+3. [Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)
---
+++ src/problems/3.longest-substring-without-repeating-characters/typescript/longest-substring-without-repeating-characters.test.ts
@@ -0,0 +1,14 @@
+import { lengthOfLongestSubstring } from './longest-substring-without-repeating-characters';
+
+describe('Longest Substring Without Repeating Characters', () => {
+  it('should return the length of the longest substring without repeating characters.', () => {
+    expect(lengthOfLongestSubstring('1')).toBe(1);
+    expect(lengthOfLongestSubstring('abcabcbb')).toBe(3);
+    expect(lengthOfLongestSubstring('bbbbb')).toBe(1);
+    expect(lengthOfLongestSubstring('pwwkew')).toBe(3);
+  });
+
+  it('should return zero with empty input', () => {
+    expect(lengthOfLongestSubstring('')).toBe(0);
+  });
+});
