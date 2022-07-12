e60f284fdad4541570e6b42e634ad4fc4f067309
Haltarys <gregoire.haardt@gmail.com>
2022-07-12 18:14:49 +0200

    feat(4.median-of-two-sorted-arrays): improve algorithm and add more tests

--- src/4.median-of-two-sorted-arrays/typescript/median-of-two-sorted-arrays.ts
+++ src/4.median-of-two-sorted-arrays/typescript/median-of-two-sorted-arrays.ts
@@ -3,9 +3,6 @@
  *
  * [4] Median of Two Sorted Arrays
  */
-
-import { findMedianOfSortedArray } from './median-of-sorted-array';
-
 /**
  * Given two sorted arrays `nums1` and `nums2` of size `m` and `n` respectively, returns the median of the two sorted arrays.
  * @param nums1 A sorted array of up to 1000 elements.
@@ -14,10 +11,33 @@ import { findMedianOfSortedArray } from './median-of-sorted-array';
  */
 // @lc code=start
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-  // Callback needed in .sort() to override the default lexicographical sort
-  const nums = nums1.concat(...nums2).sort((a, b) => a - b);
+  const length = nums1.length + nums2.length;
+  const half = Math.floor(length / 2);
 
-  return findMedianOfSortedArray(nums);
+  // If there is an odd number of values, return the value at halfpoint
+  if (length % 2 == 1) return kth(nums1, nums2, half);
+  // Else, return a average of the two
+  else return (kth(nums1, nums2, half - 1) + kth(nums1, nums2, half)) / 2;
+}
+
+function kth(nums1: number[], nums2: number[], k: number): number {
+  if (nums1.length === 0 && nums2.length === 0)
+    throw new Error('Invalid input: both arrays cannot be empty.');
+  if (nums1.length === 0) return nums2[k];
+  if (nums2.length === 0) return nums1[k];
+
+  const i1 = Math.floor(nums1.length / 2),
+    i2 = Math.floor(nums2.length / 2);
+  const m1 = nums1[i1],
+    m2 = nums2[i2];
+
+  if (i1 + i2 < k) {
+    if (m1 < m2) return kth(nums1.slice(i1 + 1), nums2, k - i1 - 1);
+    else return kth(nums1, nums2.slice(i2 + 1), k - i2 - 1);
+  } else {
+    if (m1 < m2) return kth(nums1, nums2.slice(0, i2), k);
+    else return kth(nums1.slice(0, i1), nums2, k);
+  }
 }
 // @lc code=end
 
